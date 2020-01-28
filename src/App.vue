91bd1e6ceb0ef4c72cf59a432ef1394f07c2aa75
Ben Shealy <bentshermann@gmail.com>
2020-01-27 21:03:18 -0500

    Moved custom global styles into main.css to override bootstrap

--- src/App.vue
+++ src/App.vue
@@ -7,22 +7,3 @@
 	<router-view></router-view>
 </div>
 </template>
-
-<style>
-/* typography rules */
-body {
-	font-family: monospace;
-}
-
-/* rules for button icons */
-.button-icon {
-	color: #808080;
-}
-
-.button-icon:hover,
-.button-icon:focus {
-	text-decoration: none;
-	cursor: pointer;
-	color: #303030;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './main.css'
 
 Vue.config.productionTip = false
 
