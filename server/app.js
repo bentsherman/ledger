b7da49f918c3aedd66f58f4d04f9d0fd385d465a
Ben Shealy <bentshermann@gmail.com>
2020-02-17 15:49:17 -0500

    Removed semi-colons from server code

--- server/app.js
+++ server/app.js
@@ -1,77 +1,77 @@
-const bodyParser = require("body-parser");
-const Database = require("./database");
-const express = require("express");
-const logger = require("morgan");
+const bodyParser = require("body-parser")
+const Database = require("./database")
+const express = require("express")
+const logger = require("morgan")
 
 // initialize constants
-const DOC_ROOT = "dist";
-const DB_FILENAME = "data/db.json";
-const DB_FILENAME_BACKUP = "data/db.backup.json";
+const DOC_ROOT = "dist"
+const DB_FILENAME = "data/db.json"
+const DB_FILENAME_BACKUP = "data/db.backup.json"
 
 // initialize database
-const db = new Database();
+const db = new Database()
 
-db.load(DB_FILENAME);
-db.save(DB_FILENAME_BACKUP);
+db.load(DB_FILENAME)
+db.save(DB_FILENAME_BACKUP)
 
 // initialize app
-const app = express();
+const app = express()
 
 // initialize middleware
-app.use(bodyParser.json());
-app.use(logger("dev"));
+app.use(bodyParser.json())
+app.use(logger("dev"))
 
 // define routes
-app.use("/", express.static(DOC_ROOT));
+app.use("/", express.static(DOC_ROOT))
 
 app.get("/api/transactions", function(req, res) {
-	let page = Number.parseInt(req.query.page || 0);
-	let pageSize = 100;
+	let page = Number.parseInt(req.query.page || 0)
+	let pageSize = 100
 
 	let docs = db.query("transactions")
 		.sort(function(a, b) {
 			return b["date"].localeCompare(a["date"])
 		})
-		.slice(page * pageSize, (page + 1) * pageSize);
+		.slice(page * pageSize, (page + 1) * pageSize)
 
-	res.status(200).send(docs);
-});
+	res.status(200).send(docs)
+})
 
 app.get("/api/transactions/:id", function(req, res) {
-	let doc = db.find("transactions", req.params.id);
+	let doc = db.find("transactions", req.params.id)
 
 	if ( doc ) {
-		res.status(200).send(doc);
+		res.status(200).send(doc)
 	}
 	else {
-		res.status(404).end();
+		res.status(404).end()
 	}
-});
+})
 
 app.post("/api/transactions/0", function(req, res) {
-	let doc = req.body;
+	let doc = req.body
 
-	db.push("transactions", doc);
-	db.save(DB_FILENAME);
+	db.push("transactions", doc)
+	db.save(DB_FILENAME)
 
-	res.status(200).end();
-});
+	res.status(200).end()
+})
 
 app.post("/api/transactions/:id", function(req, res) {
-	let doc = req.body;
+	let doc = req.body
 
-	db.update("transactions", req.params.id, doc);
-	db.save(DB_FILENAME);
+	db.update("transactions", req.params.id, doc)
+	db.save(DB_FILENAME)
 
-	res.status(200).end();
-});
+	res.status(200).end()
+})
 
 app.delete("/api/transactions/:id", function(req, res) {
-	db.remove("transactions", req.params.id);
-	db.save(DB_FILENAME);
+	db.remove("transactions", req.params.id)
+	db.save(DB_FILENAME)
 
-	res.status(200).end();
-});
+	res.status(200).end()
+})
 
 const computeDebt = function(debtor, creditor) {
 	// flatten transactions and sub-items into array
@@ -103,10 +103,10 @@ const computeDebt = function(debtor, creditor) {
 		}, 0)
 
 	return debt - credit
-};
+}
 
 app.get("/api/users", function(req, res) {
-	let users = db.query("users");
+	let users = db.query("users")
 
 	users.forEach(function(u1) {
 		u1.debts = users
@@ -118,26 +118,26 @@ app.get("/api/users", function(req, res) {
 				}
 			})
 			.filter(function(u2) {
-				return (u1.name !== u2.name);
+				return (u1.name !== u2.name)
 			})
 	})
 
-	res.status(200).send(users);
-});
+	res.status(200).send(users)
+})
 
 // define 404 handler
 app.use(function(req, res) {
-	res.status(404).send("Not Found: " + req.originalUrl);
-});
+	res.status(404).send("Not Found: " + req.originalUrl)
+})
 
 // define error handler
 app.use(function(err, req, res, next) {
 	if ( res.headersSent ) {
-		return next(err);
+		return next(err)
 	}
 
-	res.status(500).send(err.message);
-});
+	res.status(500).send(err.message)
+})
 
 // start HTTP web server
-app.listen(8080);
+app.listen(8080)
--- server/database.js
+++ server/database.js
@@ -1,57 +1,57 @@
 /**
  * Simple database with a JSON file as a backend.
  */
-const fs = require("fs");
+const fs = require("fs")
 
 function Database()
 {
-	let tables = {};
+	let tables = {}
 
 	this.load = function(filename) {
-		tables = JSON.parse(fs.readFileSync(filename, "utf-8"));
-	};
+		tables = JSON.parse(fs.readFileSync(filename, "utf-8"))
+	}
 
 	this.save = function(filename) {
-		let data = JSON.stringify(tables, null, "\t");
+		let data = JSON.stringify(tables, null, "\t")
 
-		fs.writeFileSync(filename, data, "utf-8");
-	};
+		fs.writeFileSync(filename, data, "utf-8")
+	}
 
 	this.query = function(tableName) {
-		return tables[tableName];
-	};
+		return tables[tableName]
+	}
 
 	this.find = function(tableName, id) {
 		return tables[tableName].find(function(d) {
-			return (d.id === id);
-		});
-	};
+			return (d.id === id)
+		})
+	}
 
 	this.push = function(tableName, doc) {
 		let max_id = tables[tableName].reduce(function(prev, d) {
-			return Math.max(prev, d.id);
-		}, 0);
+			return Math.max(prev, d.id)
+		}, 0)
 
-		doc.id = "" + (max_id + 1);
+		doc.id = "" + (max_id + 1)
 
-		tables[tableName].push(doc);
-	};
+		tables[tableName].push(doc)
+	}
 
 	this.update = function(tableName, id, update) {
 		let doc = tables[tableName].find(function(d) {
-			return (d.id === id);
-		});
+			return (d.id === id)
+		})
 
-		return Object.assign(doc, update);
-	};
+		return Object.assign(doc, update)
+	}
 
 	this.remove = function(tableName, id) {
 		let index = tables[tableName].findIndex(function(d) {
-			return (d.id === id);
-		});
+			return (d.id === id)
+		})
 
-		return tables[tableName].splice(index, 1);
-	};
+		return tables[tableName].splice(index, 1)
+	}
 }
 
-module.exports = Database;
+module.exports = Database
